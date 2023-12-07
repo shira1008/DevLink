a6d46e8c2ec7de2ffe44f32a2a5c494147e410ab
shira1008 <shaharshira1008@gmail.com>
2023-12-07 16:40:01 +0200

    added github api endpoint and education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -359,5 +361,41 @@ router.delete(
     }
   }
 );
+
+// GET api/profile/github/:username
+// get user repo for github
+// public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+      }
+
+      if (
+        response.statusCode !== 200 ||
+        !response.headers['content-type'].includes('application/json')
+      ) {
+        return res.status(404).json({ msg: 'No GitHub Profile Found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
