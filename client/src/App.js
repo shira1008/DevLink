844950e639de18bdeb579987a26b39573c6bea76
shira1008 <shaharshira1008@gmail.com>
2023-12-10 18:55:02 +0200

    checge things

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,44 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Navbar />
-      <Landing />
-    </Fragment>
+      <Routes>
+        <Route path='/' element={<Outlet />}>
+          <Route index element={<Landing />} />
+
+          {/* Apply container class to /register and /login */}
+          <Route
+            path='/register'
+            element={
+              <section className='container'>
+                <Register />
+              </section>
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              <section className='container'>
+                <Login />
+              </section>
+            }
+          />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const landing = () => {
   return (
@@ -11,12 +12,12 @@ const landing = () => {
             other developers
           </p>
           <div className='buttons'>
-            <a href='register.html' class='btn btn-primary'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
-            </a>
-            <a href='login.html' class='btn btn-light'>
+            </Link>
+            <Link to='/login' className='btn btn-light'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
