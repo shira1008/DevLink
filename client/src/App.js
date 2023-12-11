fbca16f03b671c504f83262b1a2024c836e77fb2
shira1008 <shaharshira1008@gmail.com>
2023-12-11 14:13:15 +0200

    added alert functionality for not matching passwords

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,8 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-//Redux
+import Alert from './components/layout/Alert';
+// Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -20,9 +21,16 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Outlet />}>
+          <Route
+            path='/'
+            element={
+              <>
+                <Outlet />
+                <Alert />
+              </>
+            }
+          >
             <Route index element={<Landing />} />
-
             {/* Apply container class to /register and /login */}
             <Route
               path='/register'
