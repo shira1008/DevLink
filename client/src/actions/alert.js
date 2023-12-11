6b50a08dfd85080f3f5b560cb87ae436dfaf0330
shira1008 <shaharshira1008@gmail.com>
2023-12-11 14:14:33 +0200

    alert add timout variable to function

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,14 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
 import { v4 as uuidv4 } from 'uuid';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuidv4(); // Used uuidv4 instead of uuid.v4
-  dispatch({
-    type: SET_ALERT,
-    payload: { msg, alertType, id },
-  });
+export const setAlert =
+  (msg, alertType, timeout = 3000) =>
+  (dispatch) => {
+    const id = uuidv4(); // Used uuidv4 instead of uuid.v4
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, alertType, id },
+    });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 3000);
-};
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  };
